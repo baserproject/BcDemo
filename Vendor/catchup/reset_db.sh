0b6866cd63fa2205e77719b316bb77a4bc1cf53c
gondoh <gondoh@gufii.net>
2021-04-27 05:01:37 +0900

    script追加

---
+++ Vendor/catchup/reset_db.sh
@@ -0,0 +1,6 @@
+#!/usr/local/bin/bash
+
+rm -rf /home/baser-dev/www/trial.basercms.net/app/db/sqlite/basercms.db
+cp /home/baser-dev/www/trial.basercms.net/src/db/basercms.db /home/baser-dev/www/trial.basercms.net/app/db/sqlite/basercms.db
+
+
